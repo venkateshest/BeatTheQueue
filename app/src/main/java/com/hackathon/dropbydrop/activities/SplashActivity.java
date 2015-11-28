737f1bf79054914e9623637d3c27e52ad46ce68d
srikanth <srikanth.foryou@gmail.com>
2015-11-28 16:06:03 +0530

    splashAcitivity

--- app/src/main/java/com/hackathon/dropbydrop/activities/SplashActivity.java
+++ app/src/main/java/com/hackathon/dropbydrop/activities/SplashActivity.java
@@ -1,9 +1,11 @@
-package com.hackathon.dropbydrop;
+package com.hackathon.dropbydrop.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.hackathon.dropbydrop.R;
 
 public class SplashActivity extends AppCompatActivity {
 
