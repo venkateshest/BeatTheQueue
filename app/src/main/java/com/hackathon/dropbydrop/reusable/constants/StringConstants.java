00ed24949de488487842fc8d54186f72d063a082
Iqbal Shirol <iqbal.shirol@techendeavour.com>
2015-11-30 18:29:19 +0530

    GCM - Server and Client Integration

---
+++ app/src/main/java/com/hackathon/dropbydrop/reusable/constants/StringConstants.java
@@ -0,0 +1,9 @@
+package com.hackathon.dropbydrop.reusable.constants;
+
+/**
+ * Created by ESTBLR\iqbal.shirol on 30/11/15.
+ */
+public class StringConstants {
+    public static final String GCM_SERVER_KEY = "AIzaSyCXMXJhD428UXovpWhsLddJhzcyLVCt17U";
+    public static final String SAMPLE_GCM_DEVICE_REG_ID = "APA91bEjurx6nIONEac2kFvZc8IlAXE27H1aC5hvAlFpns6gIBn0eFTbIWcGYSFo0uqaOn3rq5yi-p58V0A6r2zHgVZmtvHdqJOKoYJ39vZ6SjOXS92zD5b_NtbDezSLCuhmUtMZTP04BvQJsO6sG5bD2bzlb65xDcTjk1YruxKl5m5JhvKvRow";
+}
