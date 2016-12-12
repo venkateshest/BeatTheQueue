58335e88e54ab24d65679b9d89ff0371e3e47f37
venkateshest <venkatesh.kolla@techendeavour.com>
2016-12-12 17:33:59 +0530

    git initial chagnges

---
+++ app/src/main/java/com/hackathon/BeatTheQueue/data/CartItemsDTO.java
@@ -0,0 +1,15 @@
+package com.hackathon.BeatTheQueue.data;
+
+/**
+ * Created by venkatesh.kolla.
+ */
+public class CartItemsDTO {
+
+    public String name;
+    public String dateTime;
+    public String quantity;
+    public String price;
+    public String perKGPrice;
+
+
+}
