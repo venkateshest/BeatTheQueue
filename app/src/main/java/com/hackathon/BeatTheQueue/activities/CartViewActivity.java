cfaa5eb5784135abac3d63918a569ec0766c4d67
Durga M. Mahapatra <durga.mahapatra@techendeavour.com>
2016-12-13 14:51:38 +0530

    cartlist

--- app/src/main/java/com/hackathon/BeatTheQueue/activities/CartViewActivity.java
+++ app/src/main/java/com/hackathon/BeatTheQueue/activities/CartViewActivity.java
@@ -2,15 +2,31 @@ package com.hackathon.BeatTheQueue.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.hackathon.BeatTheQueue.R;
+import com.hackathon.BeatTheQueue.adapter.CartListAdapter;
+import com.hackathon.BeatTheQueue.data.CartItemsDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartViewActivity extends AppCompatActivity {
+
+
+    ListView cartList;
+
+    List<CartItemsDTO> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_layout);
+
+        cartList = (ListView) findViewById(R.id.ll_item_list);
+
+        CartListAdapter adapter = new CartListAdapter(this, R.layout.item_details, items);
+        cartList.setAdapter(adapter);
 
     }
 }
